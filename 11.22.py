54dc00852f1bd88a0f9e78e9c2a59ec2d97c72f7
omartinez694 <70172133+omartinez694@users.noreply.github.com>
2020-10-28 22:05:29 -0500

    Add files via upload

---
+++ 11.22.py
@@ -0,0 +1,13 @@
+l = input()
+
+l = l.split(" ")
+o = {}
+
+for each in l:
+    if each in o:
+        o[each] = o[each] + 1
+    else:
+        o[each] = 1
+
+for each in l:
+    print(each, o[each])
