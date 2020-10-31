ad905118b9729092821c2f0e0c6aa46d0237f0ce
omartinez694 <70172133+omartinez694@users.noreply.github.com>
2020-10-31 11:27:46 -0500

    Add files via upload

---
+++ 10.19.py
@@ -0,0 +1,139 @@
+
+class ItemToPurchase:
+    def __init__(self, item_name='none', item_price=0, item_quantity=0, item_description='none'):
+        self.item_name = item_name
+        self.item_price = item_price
+        self.item_quantity = item_quantity
+        self.item_description = item_description
+
+    def print_item_cost(self):
+        string = '{} {} @ ${} = ${}'.format(self.item_name, self.item_quantity, self.item_price, (self.item_quantity * self.item_price))
+        cost = self.item_quantity * self.item_price
+        return string, cost
+
+    def print_item_description(self):
+        string = '{}: {}'.format(self.item_name, self.item_description)
+        print(string, end='\n')
+        return string
+
+
+class ShoppingCart:
+    def __init__(self, customer_name='none', current_date='January 1, 2016', cart_items=[]):
+        self.customer_name = customer_name
+        self.current_date = current_date
+        self.cart_items = cart_items
+
+    def add_item(self, string):
+        print('\nADD ITEM TO CART', end='\n')
+        item_name = str(input('Enter the item name:'))
+        item_description = str(input('\nEnter the item description:'))
+        item_price = int(input('\nEnter the item price:'))
+        item_quantity = int(input('\nEnter the item quantity:'))
+        self.cart_items.append(ItemToPurchase(item_name, item_price, item_quantity, item_description))
+
+    def remove_item(self):
+        print('REMOVE ITEM FROM CART', end='\n')
+        string = str(input('Enter name of item to remove:'))
+        i = 0
+        for item in self.cart_items:
+            if (item.item_name == string):
+                del self.cart_items[i]
+                i += 1
+                flag = True
+                break
+            else:
+                flag = False
+        if (flag == False):
+            print('Item not found in cart. Nothing removed.')
+
+    def modify_item(self):
+        print('\nCHANGE ITEM QUANTITY', end='\n')
+        name = str(input('Enter the item name:'))
+        for item in self.cart_items:
+            if (item.item_name == name):
+                quantity = int(input('Enter the new quantity:'))
+                item.item_quantity = quantity
+                flag = True
+                break
+            else:
+                flag = False
+        if (flag == False):
+            print('Item not found in cart. Nothing modified')
+
+    def get_num_items_in_cart(self):
+        num_items = 0
+        for item in self.cart_items:
+            num_items = num_items + item.item_quantity
+        return num_items
+
+    def get_cost_of_cart(self):
+        total_cost = 0
+        cost = 0
+        for item in self.cart_items:
+            cost = (item.item_quantity * item.item_price)
+            total_cost += cost
+        return total_cost
+
+    def print_total(self):
+        total_cost = self.get_cost_of_cart()
+        if (total_cost == 0):
+            print('SHOPPING CART IS EMPTY')
+        else:
+            self.output_cart()
+
+    def print_descriptions(self):
+        print('\nOUTPUT ITEMS\' DESCRIPTIONS')
+        print('{}\'s Shopping Cart - {}'.format(self.customer_name, self.current_date), end='\n')
+        print('\nItem Descriptions', end='\n')
+        for item in self.cart_items:
+            print('{}: {}'.format(item.item_name, item.item_description), end='\n')
+
+    def output_cart(self):
+        new = ShoppingCart()
+        print('\nOUTPUT SHOPPING CART', end='\n')
+        print('{}\'s Shopping Cart - {}'.format(self.customer_name, self.current_date), end='\n')
+        print('Number of Items:', new.get_num_items_in_cart(), end='\n\n')
+        tc = 0
+        for item in self.cart_items:
+            print('{} {} @ ${} = ${}'.format(item.item_name, item.item_quantity,
+                                             item.item_price, (item.item_quantity * item.item_price)), end='\n')
+            tc += (item.item_quantity * item.item_price)
+        print('\nTotal: ${}'.format(tc), end='\n')
+
+
+def print_menu(ShoppingCart):
+    customer_Cart = newCart
+    string = ''
+    # declare the string menu
+    menu = ('\nMENU\n'
+            'a - Add item to cart\n'
+            'r - Remove item from cart\n'
+            'c - Change item quantity\n'
+            'i - Output items\' descriptions\n'
+            'o - Output shopping cart\n'
+            'q - Quit\n')
+    command = ''
+    while (command != 'q'):
+        string = ''
+        print(menu, end='\n')
+        command = input('Choose an option:')
+        while (command != 'a' and command != 'o' and command != 'i' and command != 'r' and command != 'c' and command != 'q'):
+            command = input('Choose an option:')
+        if (command == 'a'):
+            customer_Cart.add_item(string)
+        if (command == 'o'):
+            customer_Cart.output_cart()
+        if (command == 'i'):
+            customer_Cart.print_descriptions()
+        if (command == 'r'):
+            customer_Cart.remove_item()
+        if (command == 'c'):
+            customer_Cart.modify_item()
+
+
+customer_name = str(input('Enter customer\'s name: '))
+current_date = str(input('\nEnter today\'s date:'))
+print('\nCustomer name:', customer_name, end='\n')
+print('Today\'s date:', current_date, end='\n')
+newCart = ShoppingCart(customer_name, current_date)
+print_menu(newCart)
---
+++ 11.27.py
@@ -0,0 +1,98 @@
+def getJerseyNumber():
+    jerseyNumber = int(input("Enter new player's jersey number:"))
+    while (jerseyNumber < 0 or jerseyNumber > 99):
+        jerseyNumber = int(input("Error! Enter new player's jersey number:"))
+    return jerseyNumber
+
+def getRating():
+    rating = int(input("Enter player's rating:"))
+    while (rating < 1 or rating > 9):
+        rating = int(input("Error! Enter player's rating:"))
+    return rating
+
+def menu():
+    print("MENU")
+    print("a - Add player")
+    print("d - Remove player")
+    print("u - Update player rating")
+    print("r - Output players above a rating")
+    print("o - Output roster")
+    print("q - Quit")
+    choice = input("Choose an option: ")
+    return choice[0]
+
+def printRoster(player):
+    print("ROSTER")
+    for jersey in sorted(player):
+        print("Jersey number: ", jersey,", Rating:", player[jersey])
+
+def addPlayer(player):
+    jerseyNumber = getJerseyNumber()
+    rating = getRating()
+    player[jerseyNumber] = rating
+
+def deletePlayer(player):
+    jerseyNumber = getJerseyNumber()
+    if jerseyNumber in player:
+        player.pop(jerseyNumber)
+    else:
+        print("Jersey number not found.")
+
+def updatePlayer(player):
+    jerseyNumber = getJerseyNumber()  # get jersey number
+    if jerseyNumber in player:  # if jersey number exits in dictionary
+        rating = getRating()  # get rating
+        player[jerseyNumber] = rating
+    else:
+        print("Jersey number not found.")
+
+
+def outputRatingPlayer(player):
+    rating = getRating()  # get rating
+    print("ABOVE", rating)
+    for jersey in sorted(player):  # dictionary in sorted order
+        if (player[jersey] > rating):
+            print("Jersey number: ", jersey, ", Rating:", player[jersey])
+
+
+def getPlayerInput(player):
+    i = 0
+    while i < 5:
+        print("Enter player",(i+1),"'s jersey number:", end='\n')
+        jerseyNumber = int(input())
+        while (jerseyNumber < 0 or jerseyNumber > 99):
+            print('\n')
+            print("Error! Enter player",(i+1),"'s jersey number:", end='\n')
+            jerseyNumber = int(input())
+            print('\n')
+        print("Enter player",(i+1),"'s rating:", end='\n')
+        rating = int(input())
+        while (rating < 1 or rating > 9):
+            print()
+            print("Error! Enter player",(i+1),"'s rating:", end='\n')
+            rating = int(input())
+        player[jerseyNumber] = rating
+        i += 1
+
+if __name__ == "__main__":
+    player = {}
+    getPlayerInput(player)
+    printRoster(player)
+    while True:
+        choice = menu()
+        if choice == 'a':
+            addPlayer(player)
+        elif choice == 'd':
+            deletePlayer(player)
+        elif choice == 'u':
+            updatePlayer(player)
+        elif choice == 'r':
+            outputRatingPlayer(player)
+        elif choice == 'o':
+            printRoster(player)
+        elif choice == 'q':
+            break
+            print("")
+            break
+        else:
+            print("Invalid choice.")
