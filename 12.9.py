07a6425f7f11ebde0caabfafcdc9b0f21c99d09d
omartinez694 <70172133+omartinez694@users.noreply.github.com>
2020-11-30 19:39:16 -0600

    Add files via upload

---
+++ 12.9.py
@@ -0,0 +1,11 @@
+parts = input().split()
+name = parts[0]
+while name != '-1':
+    try:
+        age = int(parts[1]) + 1
+    except Exception as ex:
+        age = 0
+    # Get next line
+    print('{} {}'.format(name, age))
+    parts = input().split()
+    name = parts[0]
